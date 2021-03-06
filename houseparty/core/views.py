401beb4f0dd16ea6b94f4ad3bda6a56b7c76b0ae
SevimDenizYasar <>
2021-03-06 12:03:41 +0300

    api updates

--- houseparty/core/views.py
+++ houseparty/core/views.py
@@ -1,13 +1,19 @@
-from rest_framework import viewsets, mixins, permissions, pagination
+from rest_framework import status, viewsets, mixins, permissions, pagination
 
 from .serializers import ProfileSerializer, RoomSerializer
+from .permissions import ListCreateOrIsOwner
 from .models import Profile, Room
 
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.select_related('host__user').filter(is_locked=False).order_by('-created_at')
     serializer_class = RoomSerializer
+    permission_classes = (ListCreateOrIsOwner, )
 
     def perform_create(self, serializer):
         profile = self.request.user.profile
-        serializer.save(host=profile)
+        room = profile.room_set.first()
+
+        if serializer.is_valid():
+            if not room:
+                serializer.save(host=profile)
