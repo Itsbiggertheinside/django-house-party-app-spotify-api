2be50fa4d6547893b8b05692d0551c82d622c68f
SevimDenizYasar <>
2021-03-06 09:54:55 +0300

    basic routing

--- houseparty/frontend/src/App.js
+++ houseparty/frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import RoomJoinPage from './components/RoomJoinPage'
+import RoomCreatePage from './components/RoomCreatePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/room/join' component={RoomJoinPage} />
+            <Route path='/room/create' component={RoomCreatePage} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ houseparty/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RoomJoinPage extends Component {
+    render() {
+        return (
+            <div>
+                join
+            </div>
+        )
+    }
+}
